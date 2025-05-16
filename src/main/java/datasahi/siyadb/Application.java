e30d15ec0fc7859f19430d524a2823e1a99c3efc
guttulasunil <guttulasunil@gmail.com>
2025-05-16 15:27:10 +0530

    Initial commit

---
+++ src/main/java/datasahi/siyadb/Application.java
@@ -0,0 +1,10 @@
+package datasahi.siyadb;
+
+import io.micronaut.runtime.Micronaut;
+
+public class Application {
+
+    public static void main(String[] args) {
+        Micronaut.run(Application.class, args);
+    }
+}
---
+++ src/test/java/datasahi/siyadb/DatasahiSiyadbTest.java
@@ -0,0 +1,21 @@
+package datasahi.siyadb;
+
+import io.micronaut.runtime.EmbeddedApplication;
+import io.micronaut.test.extensions.junit5.annotation.MicronautTest;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Assertions;
+
+import jakarta.inject.Inject;
+
+@MicronautTest
+class DatasahiSiyadbTest {
+
+    @Inject
+    EmbeddedApplication<?> application;
+
+    @Test
+    void testItWorks() {
+        Assertions.assertTrue(application.isRunning());
+    }
+
+}
